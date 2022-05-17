ae63b1d65a2a415feb423babebeaebe9e7132280
joey57 <njorogejoey5@gmail.com>
2022-05-17 18:10:06 +0300

    register blueprint

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+class Config:
+  '''
+  '''
+  pass
+
+class ProdConfig(Config):
+  '''
+  '''
+  pass
+
+
+class DevConfig(Config):
+  '''
+  '''
+  DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
